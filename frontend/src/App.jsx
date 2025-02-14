ad64c378822a4a465421309220c1e4bd54443376
aryan-srivastava1625 <ap9140@srmist.edu.in>
2025-02-14 20:34:46 +0530

    done with react code editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
 import "prismjs/themes/prism-tomorrow.css"
-import "prismjs/components/prism-jsx"
+import Editor from "react-simple-code-editor"
 import prism from "prismjs"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [code, setCode] = useState(`function sum(){
+  return 1+1
+}`)
 
   useEffect(()=>{
     prism.highlightAll()
@@ -15,7 +18,22 @@ function App() {
     <>
     <main>
       <div className="left">
-        <div className="code"></div>
+        <div className="code">
+        <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+              }}
+            />
+        </div>
         <div className="review">Evaluate</div>
       </div>
       <div className="right"></div>
