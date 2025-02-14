98512f8aea11a45be9382e13a961901fd5c622bf
aryan-srivastava1625 <ap9140@srmist.edu.in>
2025-02-14 21:05:40 +0530

    done with project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react'
 import "prismjs/themes/prism-tomorrow.css"
 import Editor from "react-simple-code-editor"
 import prism from "prismjs"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 import axios from "axios"
 import './App.css'
 
@@ -46,9 +49,13 @@ function App() {
         onClick={reviewCode}
         className="review">Evaluate</div>
       </div>
-      <div className="right">{
-        review
-}</div>
+      <div className="right">
+        <Markdown
+        
+        rehypePlugins={[rehypeHighlight]}
+        
+        >{review}</Markdown>
+      </div>
     </main>
     </>
   )
